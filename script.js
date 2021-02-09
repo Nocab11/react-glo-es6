774ab4e1b3a6d299d174a4f05a5e00078f505dc3
Denis Berezin <nocab139@yandex.ru>
2021-02-09 23:42:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
+let nameCourse = 'Базовый React';
+let command = [];
+for (let i = 0; i < employers.length; i++) {
+	if (employers[i].length > 0 && employers[i].trim() != '') {
+		command.push(employers[i]);
+	}
+}
+for (let i = 0; i < command.length; i++) {
+	command[i] = command[i].toLowerCase().trim();
+	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
+}
+
+let data = {
+	cash: [3, 8, 3],
+	react: ['JSX', 'components', 'props', 'state', 'hooks'],
+	add: ['styled-components', 'firebase']
+};
+
+calcCash = (own) => {
+	own = own || 0;
+	let total = own;
+	for (let i = 0; i < own[1].length; i++) {
+		total += +own[1][i];
+	}
+	return total;
+}
+
+let lesson = calcCash(data.cash);
+
+makeBusiness = (director, teacher, allModule, gang, course) => {
+	teacher = teacher || 'Максим';
+	var sumTech = data.react.concat(data.add, 'и другие');
+	console.log('Стартуем новый курс: "' + course + '". Владелец: ' + director + ', преподаватель: ' + teacher + '. Всего уроков: ' + allModule + '. \nКоманда Академии: ' +
+		gang);
+	console.log('Первое что изучим будет ' + data.react[0] + ". Он очень похож на HTML!");
+	console.log('Технологии которые мы изучим: ');
+	console.log.apply(null, sumTech);
+}
+
+
+makeBusiness.apply(null, ['Артем', null, lesson, command, nameCourse]);
